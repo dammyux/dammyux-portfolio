f5bbfb0a5041e96fb24f70baf940825b34d3c8b1
dammyux <dammyadetoro27@gmail.com>
2026-09-25 21:57:02 +0100

    feat: link custom profile portrait image, optimize light mode opacity, update font loading and hero review bubbles

--- .next/static/chunks/app/case-studies/page-e059c22cdfc31a74.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220,774,979],{7740:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[619,356,441,255,358],()=>e(e.s=7740)),_N_E=e.O()}]);
